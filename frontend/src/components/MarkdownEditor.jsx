f68870cfe8abe44470e11f89402ca55aa4e9b326
Deepak Verma <0822deepakverma@gmail.com>
2024-05-11 06:12:42 +0530

    added debouce to handle flickering

--- frontend/src/components/MarkdownEditor.jsx
+++ frontend/src/components/MarkdownEditor.jsx
@@ -1,14 +1,20 @@
 import Grid from '@mui/material/Grid'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Typography from '@mui/material/Typography'
 
 const MarkdownEditor = () => {
   const [markdown, setMarkdown] = useState('<h1>Hello World </h1>')
   const [textValue, setTextValue] = useState('# Hello World')
 
-  const handleMarkdownChange = (event) => {
-    const newValue = event.target.value
-    setTextValue(newValue)
+  const debounce = (func, delay) => {
+    let timer
+    return function (...args) {
+      clearTimeout(timer)
+      timer = setTimeout(() => func.apply(this, args), delay)
+    }
+  }
+
+  const debouncedHandleMarkdownChange = debounce((newValue) => {
     fetch('https://markdown2html.onrender.com/convert', {
       method: 'POST',
       headers: {
@@ -21,20 +27,26 @@ const MarkdownEditor = () => {
         setMarkdown(data.html)
       })
       .catch((error) => console.log('error', error))
+  }, 300)
+
+  const handleMarkdownChange = (event) => {
+    const newValue = event.target.value
+    setTextValue(newValue)
   }
 
+  useEffect(() => {
+    debouncedHandleMarkdownChange(textValue)
+  }, [textValue, debouncedHandleMarkdownChange])
 
   return (
     <Grid>
-      <Grid
-       className='topbar'
-      >
+      <Grid className="topbar">
         <Typography variant="h5" sx={{ textDecoration: 'underline' }}>
           Markdown Preview Generator
         </Typography>
       </Grid>
       <Grid container>
-        <Grid xs={12} sm={12} md={6} item={true} className='markdown'>
+        <Grid xs={12} sm={12} md={6} item={true} className="markdown">
           <textarea
             placeholder="Markdown"
             rows={15}
@@ -42,7 +54,7 @@ const MarkdownEditor = () => {
             onChange={(e) => handleMarkdownChange(e)}
           />
         </Grid>
-        <Grid xs={12} sm={12} md={6} item={true} className='preview'>
+        <Grid xs={12} sm={12} md={6} item={true} className="preview">
           <div dangerouslySetInnerHTML={{ __html: markdown }}></div>
         </Grid>
       </Grid>
